9910a09976c2ec0bc1b82d2f23a79cb39744477f
JGW <wjsrjsdn12@gmail.com>
2021-01-22 17:30:27 +0900

    cross val score example

---
+++ ML/cross_val_score.py
@@ -0,0 +1,14 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score, cross_validate
+from sklearn.datasets import load_iris
+import numpy as np
+
+iris_data = load_iris()
+dt_clf = DecisionTreeClassifier(random_state=156)
+
+data = iris_data.data
+label = iris_data.target
+
+scores = cross_val_score(dt_clf, data, label, scoring='accuracy', cv=3)
+print("교차 검증 정확도:", np.round(scores, 4))
+print("평균 검증 정확도:", np.round(np.mean(scores), 4))
