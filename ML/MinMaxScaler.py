ff321a34c49ca930cb91747374356c88fa953b0e
JGW <wjsrjsdn12@gmail.com>
2021-02-02 21:31:02 +0900

    MinMaxScaler_example

---
+++ ML/MinMaxScaler.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+iris_data = iris.data
+iris_df = pd.DataFrame(data=iris_data, columns=iris.feature_names)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaler.fit(iris_df)
+iris_scaled = scaler.transform(iris_df)
+
+iris_df_scaled = pd.DataFrame(data=iris_scaled, columns=iris.feature_names)
+print('feature들의 최솟값')
+print(iris_df_scaled.min())
+print('\nfeature들의 최대값')
+print(iris_df_scaled.max())
